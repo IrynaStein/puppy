4d04013a19987484271199f3411158ccdbc6c657
Iryna Stein <80711076+IrynaStein@users.noreply.github.com>
2024-08-06 17:07:56 -0700

    add comment about tagging

--- puppy.go
+++ puppy.go
@@ -10,6 +10,11 @@ func Barks() string {
 	return "Woof! Woof! Woof!"
 }
 
+/*
+firt add, commit and push all your changes
+git tag v1.0.0 - attaches a tag to the commit
+git push origin --tag - will push all tags that have been created
+*/
 func Versioning() string {
 	fmt.Println("On version 3.0.0")
 }
