889b9e2b686e67793f006fa6a9543604775dbc4d
Iryna Stein <80711076+IrynaStein@users.noreply.github.com>
2024-08-06 17:02:19 -0700

    third

--- puppy.go
+++ puppy.go
@@ -1,9 +1,15 @@
 package puppy
 
+import "fmt"
+
 func Bark() string {
 	return "Bark"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func Versioning() string {
+	fmt.Println("On version 3.0.0")
 }
